194d121ce1f33ecffc182137cfa35e4013e67d49
Ayushi Agarwal <ayushiagarwal260103@gmail.com>
2026-02-24 19:23:14 +0530

    cookies

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -33,8 +33,8 @@ export const signUp = async (req, res) => {
 
         const token = await genToken(user._id);
         res.cookie("token", token, {
-            secure: false, //set to true in production
-            sameSite: "strict",
+            secure: true, //set to true in production
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
             httpOnly: true
         })
@@ -61,8 +61,8 @@ export const signIn = async (req, res) => {
         }
         const token = await genToken(user._id);
         res.cookie("token", token, {
-            secure: false, //set to true in production
-            sameSite: "strict",
+            secure: true, //set to true in production
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
             httpOnly: true
         })
