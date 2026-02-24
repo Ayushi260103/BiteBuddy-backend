452507b13df66fb30651f68e1253bbd3cbcc2dd9
Ayushi Agarwal <ayushiagarwal260103@gmail.com>
2026-02-24 20:04:31 +0530

    Update auth.controllers.js

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -194,6 +194,15 @@ export const googleAuth = async (req, res) => {
                 role,
                 password: null //no password since user is signing up with google
             });
+
+            const token = await genToken(user._id);
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+            return res.status(201).json(user);
         }
 
     }
